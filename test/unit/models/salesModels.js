11658d20fbca962f0f93af096b2996e4ec9f6ed9
Bruno <melobrunoti@gmail.com>
2022-04-05 10:33:42 -0300

    test rename salesModel

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -4,43 +4,44 @@ const sinon = require('sinon');
 const salesModel = require('../../../models/salesModel');
 const connection = require('../../../models/connection');
 const { fakeSales } = require('../data/mocks');
+describe('Model layer - Sales', () => { 
+  describe('get all sales', () => { 
+    before(async () => {
+      sinon.stub(connection, 'execute').resolves([fakeSales]);
+    })
+    after(() => {
+      connection.execute.restore();
+    })
 
-describe('Checks products getAll', () => { 
-	before(async () => {
-    sinon.stub(connection, 'execute').resolves([fakeSales]);
-  })
-  after(() => {
-    connection.execute.restore();
-  })
+    it('return an array with all properties', async () => {
+      const response = await salesModel.getAll();
+      expect(response[0]).to.have.a.property('saleId');
+      expect(response[0]).to.have.a.property('date');
+      expect(response[0]).to.have.a.property('productId');
+      expect(response[0]).to.have.a.property('quantity');
+    })
 
-  it('return an array with all properties', async () => {
-    const response = await salesModel.getAll();
-    expect(response[0]).to.have.a.property('saleId');
-    expect(response[0]).to.have.a.property('date');
-    expect(response[0]).to.have.a.property('productId');
-    expect(response[0]).to.have.a.property('quantity');
-  })
-
-	it('check all returns', async () => {
-		const response = await salesModel.getAll();
-		expect(response).to.have.length(3)
-    expect(response).to.be.equal(fakeSales);
-	})
-})
-
-describe('Checks  getById', () => { 
-	before(() => {
-		  sinon.stub(connection, 'execute').resolves([fakeSales[0]])
-  })
-  after(() => {
-    connection.execute.restore();
+    it('check all returns', async () => {
+      const response = await salesModel.getAll();
+      expect(response).to.have.length(3)
+      expect(response).to.be.equal(fakeSales);
+    })
   })
 
-  it('return an array with all products', async () => {
-    const response = await salesModel.getById(1);
+  describe('get sale by id', () => { 
+    before(() => {
+        sinon.stub(connection, 'execute').resolves([fakeSales[0]])
+    })
+    after(() => {
+      connection.execute.restore();
+    })
 
-    expect(response).to.be.equal(fakeSales[0]);
-  })
+    it('return a sale', async () => {
+      const response = await salesModel.getById(1);
+
+      expect(response).to.be.equal(fakeSales[0]);
+    })
+  });
 });
 
 
